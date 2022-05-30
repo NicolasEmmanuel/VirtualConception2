b73eeee330509558c56bb5d8df90a5486689157f
Nikem <emmanuel.nicolas972@gmail.com>
2022-05-30 12:20:21 -0600

    2

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+/* Route Formateur */
 Route::get('/', [FormateursController::class, 'index'])->name('formateurs');
-
 Route::get('detail/{id}', [FormateursController::class, 'detail'])->name('detail');
