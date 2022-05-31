46b440352bc84501232faa2ce0c93aee4ca49965
Slxxn <sloan.dlrz@gmail.com>
2022-05-31 14:11:36 -0400

    Layout Sloan

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,155 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/styles.css">
-    <title>Virtual</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
 
-    <nav class="navbar">
-        <div class="container">
-      
-          <div class="navbar-header">
-            <button class="navbar-toggler" data-toggle="open-navbar1">
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-            <a href="#">
-              <h4>Virtual<span>Conception</span></h4>
-            </a>
-          </div>
-      
-          <div class="navbar-menu" id="open-navbar1">
-            <ul class="navbar-nav">
-    
-              <li ><a href="#">Accueil</a></li>
-              <li><a href="#">Formation</a></li>
-              <li class="active"><a href="#">Blog</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Signin</a></li>
-    
-            </ul>
+<div class="container">
+  <div class="blog-container">
+      <div class="blog-card">
+        <div class="blog-card-header">
+          <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+        </div>
+        <div class="blog-card-body">
+          <span class="tag tag-teal">Technology</span>
+          <h4>
+            Why is the Tesla Cybertruck designed the way it
+            is?
+          </h4>
+          <p>
+            An exploration into the truck's polarising design
+          </p>
+          <div class="blog-user">
+            <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+            <div class="blog-user-info">
+              <h5>July Dec</h5>
+              <small>2h ago</small>
+            </div>
           </div>
         </div>
-    </nav>
-
-    <div class="container">
-        <div class="blog-container">
-            <div class="blog-card">
-              <div class="blog-card-header">
-                <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
-              </div>
-              <div class="blog-card-body">
-                <span class="tag tag-teal">Technology</span>
-                <h4>
-                  Why is the Tesla Cybertruck designed the way it
-                  is?
-                </h4>
-                <p>
-                  An exploration into the truck's polarising design
-                </p>
-                <div class="blog-user">
-                  <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                  <div class="blog-user-info">
-                    <h5>July Dec</h5>
-                    <small>2h ago</small>
-                  </div>
-                </div>
+      </div>
+      <div class="blog-card">
+          <div class="blog-card-header">
+            <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+          </div>
+          <div class="blog-card-body">
+            <span class="tag tag-teal">Technology</span>
+            <h4>
+              Why is the Tesla Cybertruck designed the way it
+              is?
+            </h4>
+            <p>
+              An exploration into the truck's polarising design
+            </p>
+            <div class="blog-user">
+              <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+              <div class="blog-user-info">
+                <h5>July Dec</h5>
+                <small>2h ago</small>
               </div>
             </div>
-            <div class="blog-card">
-                <div class="blog-card-header">
-                  <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
-                </div>
-                <div class="blog-card-body">
-                  <span class="tag tag-teal">Technology</span>
-                  <h4>
-                    Why is the Tesla Cybertruck designed the way it
-                    is?
-                  </h4>
-                  <p>
-                    An exploration into the truck's polarising design
-                  </p>
-                  <div class="blog-user">
-                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                    <div class="blog-user-info">
-                      <h5>July Dec</h5>
-                      <small>2h ago</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="blog-card">
-                <div class="blog-card-header">
-                  <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
-                </div>
-                <div class="blog-card-body">
-                  <span class="tag tag-teal">Technology</span>
-                  <h4>
-                    Why is the Tesla Cybertruck designed the way it
-                    is?
-                  </h4>
-                  <p>
-                    An exploration into the truck's polarising design
-                  </p>
-                  <div class="blog-user">
-                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                    <div class="blog-user-info">
-                      <h5>July Dec</h5>
-                      <small>2h ago</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="blog-card">
-                <div class="blog-card-header">
-                  <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
-                </div>
-                <div class="blog-card-body">
-                  <span class="tag tag-teal">Technology</span>
-                  <h4>
-                    Why is the Tesla Cybertruck designed the way it
-                    is?
-                  </h4>
-                  <p>
-                    An exploration into the truck's polarising design
-                  </p>
-                  <div class="blog-user">
-                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                    <div class="blog-user-info">
-                      <h5>July Dec</h5>
-                      <small>2h ago</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="blog-card">
-                <div class="blog-card-header">
-                  <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
-                </div>
-                <div class="blog-card-body">
-                  <span class="tag tag-teal">Technology</span>
-                  <h4>
-                    Why is the Tesla Cybertruck designed the way it
-                    is?
-                  </h4>
-                  <p>
-                    An exploration into the truck's polarising design
-                  </p>
-                  <div class="blog-user">
-                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                    <div class="blog-user-info">
-                      <h5>July Dec</h5>
-                      <small>2h ago</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          </div>
         </div>
-    </div>       
-</body>
-</html>
+        <div class="blog-card">
+          <div class="blog-card-header">
+            <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+          </div>
+          <div class="blog-card-body">
+            <span class="tag tag-teal">Technology</span>
+            <h4>
+              Why is the Tesla Cybertruck designed the way it
+              is?
+            </h4>
+            <p>
+              An exploration into the truck's polarising design
+            </p>
+            <div class="blog-user">
+              <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+              <div class="blog-user-info">
+                <h5>July Dec</h5>
+                <small>2h ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="blog-card">
+          <div class="blog-card-header">
+            <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+          </div>
+          <div class="blog-card-body">
+            <span class="tag tag-teal">Technology</span>
+            <h4>
+              Why is the Tesla Cybertruck designed the way it
+              is?
+            </h4>
+            <p>
+              An exploration into the truck's polarising design
+            </p>
+            <div class="blog-user">
+              <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+              <div class="blog-user-info">
+                <h5>July Dec</h5>
+                <small>2h ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="blog-card">
+          <div class="blog-card-header">
+            <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+          </div>
+          <div class="blog-card-body">
+            <span class="tag tag-teal">Technology</span>
+            <h4>
+              Why is the Tesla Cybertruck designed the way it
+              is?
+            </h4>
+            <p>
+              An exploration into the truck's polarising design
+            </p>
+            <div class="blog-user">
+              <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+              <div class="blog-user-info">
+                <h5>July Dec</h5>
+                <small>2h ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+</div>  
+    
+@endsection
+    
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,110 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/styles.css">
-    <title>Virtual</title>
-</head>
-<body>
-   <!--  <nav class="navbar">
-        <a href="">Acceuil</a>
-        <a href="">Formation</a>
-        <a href="">Les Teams</a>
-        <a href="">Blog</a>
-        <a href="">Contact</a>
-    </nav>
+@extends('layouts.app')
+@section('content')
 
-    <section class="content">
-        <h2>content</h2>
+<div class="container">
+  <section class="section1">
+      <h1>Développez vos compétences</h1>
+      <h2 id="first-h1">Vous avez envie de développer vos compétences professionnelles, vos savoir-être et savoir-faire. Embarquez avec nous, pour  connaître  le métier de développeur web et d’autres métiers.
+      L’objectif principal est : de développer vos compétences, vos connaissances et également obtenir votre diplôme !
+      </h2>
+      <div >
+           <ul class="formationChip">
+               <li class="chip">Nos formation:</li>
+               <li class="chip">Concepteur Developpeur d'Application</li>
+               <li class="chip">Concepteur Web et Web Mobile</li>
+               <li class="chip">Réseau Télécomunication</li>
+           </ul>
+          
+      </div>
+  </section>
+  
+  <section>
+  <h1 id="second-h1">I really hope this helps me learn vanilla JavaScript better</h1>
+  </section>
+  
+  <section>
+  <h1 id="third-h1">I think it will honestly</h1>
+  </section>
+  
+  <section>
+  <h1 id="fourth-h1">Or maybe it will frustrate me till the end of time</h1>
+  </section>
+  
+  <section>
+  <h1 id="fifth-h1">Even if that does happen, I'll still keep trying</h1>
+  </section>
 
+  <script type="text/javascript" src="assets/background.js"></script>
 
-
-    </section>
-        
-    </div>-->
-
-
-    <nav class="navbar">
-        <div class="containerNav">
-      
-          <div class="navbar-header">
-            <button class="navbar-toggler" data-toggle="open-navbar1">
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-            <a href="#">
-              <h4>Virtual<span>Conception</span></h4>
-            </a>
-          </div>
-      
-          <div class="navbar-menu" id="open-navbar1">
-            <ul class="navbar-nav">
-              <li class="active"><a href="#">Accueil</a></li>
-              
-              <li><a href="#">Formation</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Signin</a></li>
-
-
-              <!-- <li class="navbar-dropdown">
-                <a href="#" class="dropdown-toggler" data-dropdown="blog">
-                    Les Teams <i class="fa fa-angle-down"></i>
-                </a>
-                <ul class="dropdown" id="blog">
-                  <li><a href="#">Some category</a></li>
-                  <li><a href="#">Some another category</a></li>
-                  <li class="separator"></li>
-                  <li><a href="#">Seprated link</a></li>
-                  <li class="separator"></li>
-                  <li><a href="#">One more seprated link.</a></li>
-                </ul>
-              </li> -->
-
-
-              
-            </ul>
-          </div>
-        </div>
-    </nav>
-        <div class="container">
-            <section class="section1">
-                <h1>Développez vos compétences</h1>
-                <h2 id="first-h1">Vous avez envie de développer vos compétences professionnelles, vos savoir-être et savoir-faire. Embarquez avec nous, pour  connaître  le métier de développeur web et d’autres métiers.
-                L’objectif principal est : de développer vos compétences, vos connaissances et également obtenir votre diplôme !
-                </h2>
-                <div >
-                     <ul class="formationChip">
-                         <li class="chip">Nos formation:</li>
-                         <li class="chip">Concepteur Developpeur d'Application</li>
-                         <li class="chip">Concepteur Web et Web Mobile</li>
-                         <li class="chip">Réseau Télécomunication</li>
-                     </ul>
-                    
-                </div>
-            </section>
-            
-            <section>
-            <h1 id="second-h1">I really hope this helps me learn vanilla JavaScript better</h1>
-            </section>
-            
-            <section>
-            <h1 id="third-h1">I think it will honestly</h1>
-            </section>
-            
-            <section>
-            <h1 id="fourth-h1">Or maybe it will frustrate me till the end of time</h1>
-            </section>
-            
-            <section>
-            <h1 id="fifth-h1">Even if that does happen, I'll still keep trying</h1>
-            </section>
-    
-            <script type="text/javascript" src="assets/background.js"></script>
-        
-        
-</body>
-</html>
+@endsection
